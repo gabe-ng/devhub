2fe0a8a1413bcc4d6f837ea265b122a9b81ba4a2
Gabriel Ng <gabrielng15@gmail.com>
2018-10-03 15:23:04 -0700

    #11 added favorited page

--- src/containers/Favorites.js
+++ src/containers/Favorites.js
@@ -2,7 +2,32 @@ import React, { Component } from "react";
 
 class Favorites extends Component {
   render() {
-    return <div>Favorites Section</div>;
+    const savedContainer = {
+      backgroundColor: "whitesmoke",
+      height: "84vh"
+    }
+
+    return <div>
+        <div className="container-fluid">
+          <h3 className="text-center pt-3 pb-3">Favorites</h3>
+        </div>
+        <div className="container-fluid" style={savedContainer}>
+          <div className="container pt-4">
+            <h6 className="pl-4 pb-2 font-weight-bold">Starred Jobs</h6>
+            <ul class="list-group">
+              <li class="list-group-item">test</li>
+              <li class="list-group-item">test</li>
+              <li class="list-group-item">test</li>
+            </ul>
+            <h6 className="pl-4 pt-5 pb-2 font-weight-bold">Starred Articles</h6>
+            <ul class="list-group">
+              <li class="list-group-item">test</li>
+              <li class="list-group-item">test</li>
+              <li class="list-group-item">test</li>
+            </ul>
+          </div>
+        </div>
+      </div>;
   }
 }
 
