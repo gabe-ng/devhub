962175881d09c615ca76dd1d77f1b544b9ccbd9f
Gabriel Ng <gabrielng15@gmail.com>
2018-10-04 15:06:23 -0700

    #18 fixed https in article api request

--- src/models/Articles.js
+++ src/models/Articles.js
@@ -3,14 +3,14 @@ import axios from "axios";
 export default class ArticleModels {
 
     static searchQuery(topic) {
-        let url = `http://hn.algolia.com/api/v1/search_by_date?query=${topic}&tags=story`;
+        let url = `https://hn.algolia.com/api/v1/search_by_date?query=${topic}&tags=story`;
         let request = axios.get(url);
 
         return request;
     }
 
     static initalLoad() {
-        let url = `http://hn.algolia.com/api/v1/search_by_date?tags=story`;
+        let url = `https://hn.algolia.com/api/v1/search_by_date?tags=story`;
         let request = axios.get(url);
 
         return request;
