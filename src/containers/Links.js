492ba45606da1a3561be8ca4ee2b2bbb09ecf73b
Gabriel Ng <gabrielng15@gmail.com>
2018-10-05 11:26:30 -0700

    #20 added links to link page

--- src/containers/Links.js
+++ src/containers/Links.js
@@ -4,26 +4,54 @@ class Links extends Component {
   render() {
     const savedContainer = {
       backgroundColor: "whitesmoke",
-      minHeight: "84vh"
+      minHeight: "80vh"
     }
 
     return <div>
         <div className="container-fluid">
           <h3 className="text-center pt-3 pb-3">Links</h3>
+          <p className="text-center">
+            Some helpful links for all your Developer needs.
+          </p>
         </div>
         <div className="container-fluid" style={savedContainer}>
           <div className="container pt-4">
-            <h6 className="pl-4 pb-2 font-weight-bold">Starred Jobs</h6>
             <ul class="list-group">
-              <li class="list-group-item">test</li>
-              <li class="list-group-item">test</li>
-              <li class="list-group-item">test</li>
-            </ul>
-            <h6 className="pl-4 pt-5 pb-2 font-weight-bold">Starred Articles</h6>
-            <ul class="list-group">
-              <li class="list-group-item">test</li>
-              <li class="list-group-item">test</li>
-              <li class="list-group-item">test</li>
+              <li class="list-group-item">
+                <a href="https://github.com/dypsilon/frontend-dev-bookmarks" target="_blank" rel="noopener noreferrer">
+                  Frontend Dev Bookmarks
+                </a>
+              </li>
+              <li class="list-group-item">
+                <a href="https://www.keycdn.com/blog/web-development-tools" target="_blank" rel="noopener noreferrer">
+                  Web Development Tools
+                </a>
+              </li>
+              <li class="list-group-item">
+                <a href="https://hackernoon.com/the-top-66-developer-resources-from-2017-e82531365e6d" target="_blank" rel="noopener noreferrer">
+                  Top Developer Resources from 2017
+                </a>
+              </li>
+              <li class="list-group-item">
+                <a href="https://developers.google.com/web/" target="_blank" rel="noopener noreferrer">
+                  Google Developers
+                </a>
+              </li>
+              <li class="list-group-item">
+                <a href="https://skillcrush.com/2017/06/14/web-developer-free-resources/" target="_blank" rel="noopener noreferrer">
+                  Web Developer Resources
+                </a>
+              </li>
+              <li class="list-group-item">
+                <a href="https://medium.com/dvlprstash/free-resources-for-developers-dec-2017-d23eddd83f5" target="_blank" rel="noopener noreferrer">
+                  Free Open Source Resources
+                </a>
+              </li>
+              <li class="list-group-item">
+                <a href="https://www.hongkiat.com/blog/web-developer-resources-compilation/" target="_blank" rel="noopener noreferrer">
+                  A Mega Compilation of Developer Resources
+                </a>
+              </li>
             </ul>
           </div>
         </div>
